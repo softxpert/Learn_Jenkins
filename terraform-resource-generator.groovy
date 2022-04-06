af4e6406e7c63cbe59e6f895f87af2bb8ac630e4
Thomas Mundt <mundt_thomas@yahoo.de>
2022-04-06 11:44:41 +0200

    Update terraform-resource-generator.groovy

--- terraform-resource-generator.groovy
+++ terraform-resource-generator.groovy
@@ -1,66 +1,58 @@
 
 
-IDENTIFIER = "${params.IDENTIFIER}".trim()
-
-
-
-
-/**
- * Adds a string parameter
- *
- * @return array
- */
-def getIdentifierParameter() {
-    setEnvVars()
-    return [$class: 'ValidatingStringParameterDefinition',
-            //defaultValue: 'ed4-helloworld',
-            description: """
-<b>Name of Service, use dashes here if required!</b>
-""" ,
-            regex: '^[a-z][a-z0-9-]{5,31}$',
-            name: 'IDENTIFIER'
-    ]
-}
-
-def getSqsParameter() {
-    return [$class: 'BooleanParameterDefinition',
-            description: """
-<b>Add sqs & sns to module configuration</b>
-""" ,
-            name: 'SQS'
-    ]
-}
 
 
 
 
 
-def setEnvVars() {
-  echo "setEnvVars" 
-}
-
-def getParameters() {
-    return [
-        getSqsParameter()
-        
-
-    ]
-}
 
 
-node {
-  
-  properties([
-    parameters(
-      getParameters()
-    )
-  ])
-  
-  stage('pre-condition-check') {
-    echo "pre-condition-check"
-  }
-  
-  stage('build') {
-    echo "build"
-  }
+//def setEnvVars() {
+//  echo "setEnvVars" 
+//}
+
+//def getParameters() {
+//    return [
+//        getSqsParameter()
+//        
+//
+//    ]
+//}
+
+
+pipeline {
+    agent any
+    stages {
+        stage('Setup parameters') {
+            steps {
+                script { 
+                    properties([
+                        parameters([
+                            choice(
+                                choices: ['ONE', 'TWO'], 
+                                name: 'PARAMETER_01'
+                            ),
+                            booleanParam(
+                                defaultValue: true, 
+                                description: '', 
+                                name: 'BOOLEAN'
+                            ),
+                            text(
+                                defaultValue: '''
+                                this is a multi-line 
+                                string parameter example
+                                ''', 
+                                 name: 'MULTI-LINE-STRING'
+                            ),
+                            string(
+                                defaultValue: 'scriptcrunch', 
+                                name: 'STRING-PARAMETER', 
+                                trim: true
+                            )
+                        ])
+                    ])
+                }
+            }
+        }
+    }   
 }
