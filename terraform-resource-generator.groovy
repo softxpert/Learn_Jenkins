97fe5daf4ff6f37a569c421f1fbcad83234b4099
Thomas Mundt <mundt_thomas@yahoo.de>
2022-04-06 12:16:52 +0200

    Update terraform-resource-generator.groovy

--- terraform-resource-generator.groovy
+++ terraform-resource-generator.groovy
@@ -16,7 +16,7 @@ pipeline {
         //string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
         //choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: '')
         //booleanParam(name: 'executeTests', defaultValue: true, description: 'Bla')
-        validatingString(name: "IDENTIFIER", defaultValue: "", regex: /^[a-z][a-z0-9-]{5,31}$/, failedValidationMessage: 'Value entered does not match regular expression: ^[a-z][a-z0-9-]{5,31}$', description: "<b>Name of Service, use dashes here if required!</b>")
+        validatingString(name: "IDENTIFIER", defaultValue: "", regex: /^[a-z][a-z0-9-]{5,31}$/, failedValidationMessage: 'Value entered does not match regular expression: ^[a-z][a-z0-9-]{5,31}$', description: "Name of Service, use dashes here if required!")
     }
     
     stages {
