01b1eb3da4a58f084e5ee231a444421a1f67f295
Thomas Mundt <mundt_thomas@yahoo.de>
2022-04-06 12:09:28 +0200

    Update terraform-resource-generator.groovy

--- terraform-resource-generator.groovy
+++ terraform-resource-generator.groovy
@@ -16,7 +16,7 @@ pipeline {
         string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
         choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: '')
         booleanParam(name: 'executeTests', defaultValue: true, description: 'Bla')
-        validatingString(name: "test", defaultValue: "", regex: /^abc-[0-9]+$/, failedValidationMessage: "Validation failed!", description: "ABC")
+        validatingString(name: "IDENTIFIER", defaultValue: "", regex: /^[a-z][a-z0-9-]{5,31}$/, failedValidationMessage: "Validation failed!", description: "Name of Service, use dashes here if required!")
     }
     
     stages {
