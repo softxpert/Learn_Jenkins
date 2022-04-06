919891ec5bf9454ac34dac3cfb562bafccff455f
Thomas Mundt <mundt_thomas@yahoo.de>
2022-04-06 11:57:54 +0200

    Update terraform-resource-generator.groovy

--- terraform-resource-generator.groovy
+++ terraform-resource-generator.groovy
@@ -8,14 +8,14 @@
 
 
 
-
-
 pipeline {
     
     agent any
     
     parameters {
         string(name: 'Greeting', defaultValue: 'Hello', description: 'How should I greet the world?')
+        choice(name: 'VERSION', choices: ['1.1.0', '1.2.0', '1.3.0'], description: '')
+        booleanParam(name: 'executeTests', defaultValue; true, description: '')
     }
     
     stages {
