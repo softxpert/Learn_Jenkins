f176ab8ce0b1d09ebc9877b5976026548c9ffb3a
Thomas Mundt <mundt_thomas@yahoo.de>
2022-04-06 11:26:41 +0200

    Update terraform-resource-generator.groovy

--- terraform-resource-generator.groovy
+++ terraform-resource-generator.groovy
@@ -1,7 +1,52 @@
 
 
 
+
+
+/**
+ * Adds a string parameter
+ *
+ * @return array
+ */
+def getIdentifierParameter() {
+    setEnvVars()
+  
+#    return [$class: 'ValidatingStringParameterDefinition',
+#            //defaultValue: 'ed4-helloworld',
+#            description: """
+#<b>Name of Service, use dashes here if required!</b>
+#""" ,
+#            regex: '^[a-z][a-z0-9-]{5,31}$',
+#            name: 'IDENTIFIER'
+#    ]
+}
+
+
+def setEnvVars() {
+  echo "setEnvVars" 
+}
+
+def getParameters() {
+    return [
+        getIdentifierParameter(),
+        #getBucketParameter(),
+        #getDatabaseParameter(),
+        #getServiceAccountParameter(),
+        #getCognitoParameter(),
+        #getSqsParameter(),
+        #getSnsParameter(),
+
+    ]
+}
+
+
 node {
+  
+  properties([
+    parameters(
+      getParameters()
+    )
+  ])
   
   stage('pre-condition-check') {
     echo "pre-condition-check"
